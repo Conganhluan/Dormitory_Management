61cd167df362200b25abc331fe3f4091969cebab
Dong Minh Thien <thien.dong0603@hcmut.edu.vn>
2023-04-11 19:37:09 +0700

    debugs

--- ManagerMainScreen1.Designer.cs
+++ ManagerMainScreen1.Designer.cs
@@ -22,14 +22,14 @@ namespace DormitoryManagment {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class ManagerMainScreen {
+    public partial class ManagerMainScreen {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal ManagerMainScreen() {
+        internal ManagerMainScreen(int a) {
         }
         
         /// <summary>
