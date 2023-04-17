5c712ed86ee4087cd4852cb97c9870e45652a7aa
Dong Minh Thien <thien.dong0603@hcmut.edu.vn>
2023-04-18 01:13:27 +0700

    Admin_AddUsers

--- Admin_AddUsers.cs
+++ Admin_AddUsers.cs
@@ -16,5 +16,20 @@ namespace DormitoryManagment
         {
             InitializeComponent();
         }
+
+        private void save_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (managerrb.Checked)
+                Program.admin.AddManagers(filepath.Text);
+            else if (studentrb.Checked)
+                Program.admin.AddStudents(filepath.Text);
+            else MessageBox.Show("Please enter the file path and choose Users' type!");
+        }
+
+        private void RETURN_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            DMForm f = new AdminMainScreen();
+            Navigate(ref f);
+        }
     }
 }
